2662709a48b36f69ddbf13e863b7ae15569d1242
LesageAdrien <158847908+LesageAdrien@users.noreply.github.com>
2024-02-06 17:12:39 +0100

    Update Equations de Airy.py

--- Codes/Equations de Airy.py
+++ Codes/Equations de Airy.py
@@ -80,7 +80,7 @@ while t<T:
     plt.clf()
     plt.ylim(-1,1)
     plt.title("Transformée de fourier")
-    plt.plot(xfreq[:N//2], np.real(y[:N//2]),"b", label= "Re(Û)") #la fonction étant réelle, sa FFT est une fonction paire. On ne regardera alors que la restriction à R+ de cette fonction.
+    plt.plot(xfreq[:N//2], np.real(y[:N//2]),"b", label= "Re(Û)") #la fonction U étant à valeurs réelles, sa FFT est une fonction paire. On ne regardera alors que la restriction à R+ de cette fonction.
     plt.plot(xfreq[:N//2], np.imag(y[:N//2]),"r", label= "Im(Û)") 
     plt.plot(xfreq[:N//2], np.abs(y[:N//2]),"k", label= "|Û|")
     plt.xlabel("$\\xi $")
@@ -88,13 +88,15 @@ while t<T:
     plt.show()
     plt.pause(0.01)
     
-    plt.figure(2) #On se propose de regarder la vitesse de rotation de chaque Û(x) au cours du temps. Autrement dit, on regarde la  dérivée de arg(Û) par rapport au temps.
+    # Le module de Û ne varie pas, mais on distingue toute de même une variation de arg(Û) au cour du temps.
+    plt.figure(2) # C'est pour cela qu'on se propose ici de regarder la  dérivée de arg(Û) par rapport au temps (i.e. la vitesse de déphasage des harmoniques de U en fonction de ses fréquences)
     plt.get_current_fig_manager().window.setGeometry(560,30,870,700)
     plt.clf()
-    plt.title("Dérivée temporelle du déphasage en fonction de la fréquence (en rad/s), au temps t="+str(round(t,2)) )
+    plt.title("Dérivée temporelle du déphasage (en rad/s) en fonction de la fréquence $\\xi$, au temps t="+str(round(t,2)) )
     plt.plot(xfreq[:N//5], np.angle(fft(U)[:N//5]/fft(Unew)[:N//5])/dt) #On ne regardera que sur les premières fréquences car sinon on risque la division par 0.
     plt.xlabel("$\\xi $")
     plt.show()
+    plt.pause(0.01)
     
     """Actualisation de U et de t"""
     U = np.copy(Unew)
